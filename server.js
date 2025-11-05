0855e29a261f0bddc5580f5d472e6a5524774334
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-11-05 22:50:59 +0000

    Add cors to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import routes from './src/routes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config()
 
