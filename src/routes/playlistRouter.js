d870977f87ee1909a1aedc8140a1d7f88778f9c2
timtim40a <timtim40a@gmail.com>
2025-11-06 16:52:21 +0000

    GET /api/playlists route done

--- src/routes/playlistRouter.js
+++ src/routes/playlistRouter.js
@@ -4,10 +4,17 @@ import {
     getTestPlaylists,
     updateTestPlaylist,
 } from '../test-values/playlistTestValues.js'
+import { db } from '../db/db.js'
 
 const playlistRouter = Router()
 
-playlistRouter.get('/', getTestPlaylists)
+playlistRouter.get('/', async (req, res) => {
+    const result = await db.query(
+        'SELECT p.slug, p.title, p.summary, p.date_created FROM playlists AS p'
+    )
+    res.status(200).json(result.rows)
+})
+
 playlistRouter.get('/:slug', getTestPlaylist)
 playlistRouter.patch('/:slug', updateTestPlaylist)
 playlistRouter.delete('/:slug', (req, res) => {
