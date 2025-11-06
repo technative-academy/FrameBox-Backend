00ce9c0f4e4eeed2c81867206a7d9a9918b2f462
timtim40a <timtim40a@gmail.com>
2025-11-06 13:21:49 +0000

    Adjusted for new error handling settings

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -9,14 +9,8 @@ import { db } from '../db/db.js'
 const movieRouter = Router()
 
 movieRouter.get('/', async (req, res) => {
-    try {
-        const result = await db.query('SELECT * FROM movies')
-        res.status(200).json(result.rows)
-    } catch (err) {
-        res.status(500).json({
-            err: 'Encountered a problem while reaching to the database.',
-        })
-    }
+    const result = await db.query('SELECT * FROM movies')
+    res.status(200).json(result.rows)
 })
 movieRouter.get('/:slug', getTestMovie)
 movieRouter.patch('/:slug', updateTestMovie)
