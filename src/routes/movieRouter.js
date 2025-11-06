b77f56e5d71fe03c04231a57bbf9c72a2466268c
timtim40a <timtim40a@gmail.com>
2025-11-06 13:55:30 +0000

    GET /api/movies/:slug route done

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -8,12 +8,27 @@ import { db } from '../db/db.js'
 
 const movieRouter = Router()
 
+//get all movies
 movieRouter.get('/', async (req, res) => {
     const result = await db.query('SELECT * FROM movies')
     res.status(200).json(result.rows)
 })
-movieRouter.get('/:slug', getTestMovie)
-movieRouter.patch('/:slug', updateTestMovie)
+
+//get movie by name (slug)
+movieRouter.get('/:slug', async (req, res) => {
+    const slug = req.params.slug
+    const result = await db.query('SELECT * FROM movies WHERE slug = $1', [
+        slug,
+    ])
+    result.rowCount == 0
+        ? res.status(404).json({
+              status: 404,
+              message: `entry with the slug "${slug}" was not found.`,
+          })
+        : null
+    res.status(200).json(result.rows)
+})
+
 movieRouter.delete('/:slug', (req, res) => {
     res.status(204).end()
 })
