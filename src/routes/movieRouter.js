5e3e8e859a06f4753e6dfaeae4c1195c8e617b81
timtim40a <timtim40a@gmail.com>
2025-11-06 16:01:38 +0000

    PATCH /api/movies/:slug route done

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -5,21 +5,25 @@ import {
     updateTestMovie,
 } from '../test-values/movieTestValues.js'
 import { db } from '../db/db.js'
+import slugify from 'slugify'
 
 const movieRouter = Router()
 
 //get all movies
 movieRouter.get('/', async (req, res) => {
-    const result = await db.query('SELECT * FROM movies')
+    const result = await db.query(
+        'SELECT m.slug, m.title, m.description, m.img FROM movies AS m'
+    )
     res.status(200).json(result.rows)
 })
 
 //get movie by name (slug)
 movieRouter.get('/:slug', async (req, res) => {
     const slug = req.params.slug
-    const result = await db.query('SELECT * FROM movies WHERE slug = $1', [
-        slug,
-    ])
+    const result = await db.query(
+        'SELECT m.slug, m.title, m.description, m.img FROM movies AS m WHERE slug = $1',
+        [slug]
+    )
     result.rowCount == 0
         ? res.status(404).json({
               status: 404,
@@ -29,11 +33,52 @@ movieRouter.get('/:slug', async (req, res) => {
     res.status(200).json(result.rows)
 })
 
-movieRouter.delete('/:slug', (req, res) => {
-    res.status(204).end()
-})
-movieRouter.post('/', (req, res) => {
-    res.status(201).json({ message: 'Movie created' })
+//update movie by name (slug)
+movieRouter.patch('/:slug', async (req, res) => {
+    const slug = req.params.slug
+    const incomingPatch = req.body
+
+    const fields = []
+    const values = []
+
+    if (incomingPatch.title) {
+        fields.push(`title = $${fields.length + 1}`)
+        values.push(incomingPatch.title)
+        fields.push(`slug = $${fields.length + 1}`)
+        values.push(
+            slugify(incomingPatch.title, {
+                replacement: '-',
+                remove: /[*+~.()'"!:@]/g,
+                lower: true,
+            })
+        )
+
+        // a risk of potentially same slugs arises here: to be solved later
+    }
+    if (incomingPatch.description) {
+        fields.push(`description = $${fields.length + 1}`)
+        values.push(incomingPatch.description)
+    }
+    if (incomingPatch.img) {
+        fields.push(`img = $${fields.length + 1}`)
+        values.push(incomingPatch.img)
+    }
+    if (fields.length === 0) {
+        return res.status(400).json({ message: 'No valid fields to update.' })
+    }
+
+    //pushing initial slug to specify which entry to patch
+    values.push(slug)
+
+    const sql = `
+    UPDATE movies 
+    SET ${fields.join(', ')} 
+    WHERE "slug" = $${values.length}
+    `
+
+    const result = await db.query(sql, values)
+
+    res.status(201).json(`Entry "${slug}" has been succesfully updated.`)
 })
 
 export default movieRouter
