348080f3e1212ec46c5ccd19b4c032eb2448bf00
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-11-06 14:13:01 +0000

    Integrate Swagger Doc into application on path '/api/'

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,15 @@ import movieRouter from './routes/movieRouter.js'
 import authRouter from './routes/authRouter.js'
 import playlistRouter from './routes/playlistRouter.js'
 import userRouter from './routes/userRouter.js'
+import swaggerUi from 'swagger-ui-express'
+import fs from 'fs'
+import YAML from 'yaml'
 
+const file = fs.readFileSync('src/docs/FrameBox.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
 const router = Router()
 
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 router.use('/auth', authRouter)
 router.use('/movies', movieRouter)
 router.use('/playlists', playlistRouter)
