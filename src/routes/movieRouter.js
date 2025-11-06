3765c99102f980b746403a86c8390ea68cbe3bde
timtim40a <timtim40a@gmail.com>
2025-11-06 11:33:05 +0000

    Added one test route to try the DB connection

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -4,10 +4,20 @@ import {
     getTestMovies,
     updateTestMovie,
 } from '../test-values/movieTestValues.js'
+import { db } from '../db/db.js'
 
 const movieRouter = Router()
 
-movieRouter.get('/', getTestMovies)
+movieRouter.get('/', async (req, res) => {
+    try {
+        const result = await db.query('SELECT * FROM movies')
+        res.status(200).json(result.rows)
+    } catch (err) {
+        res.status(500).json({
+            err: 'Encountered a problem while reaching to the database.',
+        })
+    }
+})
 movieRouter.get('/:slug', getTestMovie)
 movieRouter.patch('/:slug', updateTestMovie)
 movieRouter.delete('/:slug', (req, res) => {
