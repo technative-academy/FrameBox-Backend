99d98e9fbeccf5554ed14f5c623e74b22a22ac4c
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-11-07 22:26:57 +0000

    Add line explaining usage for all resources

--- src/middleware/slugIdentifier.js
+++ src/middleware/slugIdentifier.js
@@ -1,6 +1,8 @@
 import slugify from 'slugify'
 
 export function slugIdentifier(req, res, next) {
+    // specify title and username so it works for movies, playlists and users
+
     if (['title', 'username'].some((key) => key in req.body)) {
         const title = req.body.title || req.body.username
         const slug = slugify(title, {
