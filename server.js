243f0171a22bbb2ae3d9b3581b14c0aa02666597
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-11-07 19:01:33 +0000

    Amended API docs path and added splat route for 404 handling.

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use('/api', routes)
 app.get('/test', (req, res) => {
     res.status(200).json({ it_is: 'working!' })
 })
+app.all('*splat', (req, res) => {
+    res.status(404).send('404 Page Not Found')
+})
 
 // Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
 app.use((err, req, res, next) => {
