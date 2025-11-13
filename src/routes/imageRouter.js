85c2b81a6fb6bbd39f4247881de2aaff2d97d4e2
timtim40a <timtim40a@gmail.com>
2025-11-13 16:11:42 +0000

    Add formatting to the images uploaded

--- src/routes/imageRouter.js
+++ src/routes/imageRouter.js
@@ -4,6 +4,7 @@ import { v2 as cloudinary } from 'cloudinary'
 import dotenv from 'dotenv'
 import { db } from '../db/db.js'
 import fs from 'fs'
+import authenticateToken from '../middleware/auth.js'
 
 dotenv.config()
 const imageRouter = Router()
@@ -16,15 +17,19 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
-imageRouter.get('/', (req, res) => {
-    res.send(`
+imageRouter.get(
+    '/',
+    //authenticateToken,
+    (req, res) => {
+        res.send(`
     <h1>File Upload Demo</h1>
     <form action="images/playlists/inception" method="post" enctype="multipart/form-data">
         <input type="file" name="image" />
         <button type="submit">Upload</button>
     </form>
     `)
-})
+    }
+)
 
 // Upload endpoint
 imageRouter.post(
@@ -35,10 +40,16 @@ imageRouter.post(
             folder: 'user_uploads',
         })
         const slug = req.params.slug
-        const url = result.secure_url
         const table = `playlists`
 
         await fs.promises.unlink(req.file.path)
+
+        const url = cloudinary.url(result.public_id, {
+            transformation: [
+                { height: 700, width: 700, crop: 'fill' },
+                { quality: 'auto:low' },
+            ],
+        })
 
         const resultDb = await db.query(
             `UPDATE ${table} SET img = $1 WHERE slug = $2`,
@@ -53,10 +64,18 @@ imageRouter.post('/movies/:slug', upload.single('image'), async (req, res) => {
         folder: 'user_uploads',
     })
     const slug = req.params.slug
-    const url = result.secure_url
     const table = `movies`
 
     await fs.promises.unlink(req.file.path)
+
+    const url = cloudinary.url(result.public_id, {
+        transformation: [
+            {
+                width: 400,
+                height: 450,
+            },
+        ],
+    })
 
     const resultDb = await db.query(
         `UPDATE ${table} SET img = $1 WHERE slug = $2`,
