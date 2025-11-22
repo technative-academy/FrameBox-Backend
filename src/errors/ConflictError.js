860280379f8bce36308fc9932b360314ddab52f8
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-11-22 22:34:17 +0000

    Alter errors to send JSON responses

--- src/errors/ConflictError.js
+++ src/errors/ConflictError.js
@@ -4,6 +4,10 @@ class ConflictError extends Error {
         this.statusCode = 409
         this.name = 'ConflictError'
     }
+
+    toJSON() {
+        return { error: this.name, code: this.statusCode, message: this.message }
+    }
 }
 
 export default ConflictError
--- src/errors/UnauthorisedError.js
+++ src/errors/UnauthorisedError.js
@@ -4,6 +4,10 @@ class UnauthorisedError extends Error {
         this.statusCode = 401
         this.name = 'UnauthorisedError'
     }
+
+    toJSON() {
+        return { error: this.name, code: this.statusCode, message: this.message }
+    }
 }
 
 export default UnauthorisedError
