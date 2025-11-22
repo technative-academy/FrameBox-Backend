f013dc2fb082d47ae9e28084c49c4843de5e7b76
Matt Hemstock <227414719+waker-btn@users.noreply.github.com>
2025-11-22 22:32:53 +0000

    Replace InvalidDataError with BadRequestError

---
+++ src/errors/BadRequestError.js
@@ -0,0 +1,13 @@
+class BadRequestError extends Error {
+    constructor(message) {
+        super(message)
+        this.statusCode = 400
+        this.name = 'BadRequestError'
+    }
+
+    toJSON() {
+        return { error: this.name, code: this.statusCode, message: this.message }
+    }
+}
+
+export default BadRequestError
--- src/errors/InvalidDataError.js
+++
@@ -1,9 +0,0 @@
-class InvalidDataError extends Error {
-    constructor(message) {
-        super(message)
-        this.statusCode = 400
-        this.name = 'InvalidDataError'
-    }
-}
-
-export default InvalidDataError
